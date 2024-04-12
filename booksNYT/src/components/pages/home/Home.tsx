1b6effcbb71b0a626ee9526f993441095fc17fde
kseniia_kladova <Kladova.ks@yandex.ru>
2024-04-13 02:58:45 +0500

    cleaning

--- booksNYT/src/components/pages/home/Home.tsx
+++ booksNYT/src/components/pages/home/Home.tsx
@@ -1,18 +1,11 @@
 import { List } from '../../list/List'
 import { InputSearch } from '../../input-search/inputSearch'
-import PropTypes from 'prop-types';
 import { useGetBooksCategoryQuery } from '../../../app/api';
 import { useBookCategory } from '../../select/SelectContext';
 
 export default function Home() {
     const { selectedCategory } = useBookCategory();
     const { data, isLoading } = useGetBooksCategoryQuery(selectedCategory.value);
-
-    // if (data === undefined) return (  <div>
-    //     <InputSearch />
-    //     <h1>Best Sellers</h1>
-    //     <p>Loading...</p>
-    // </div>)
 
     if (data === undefined || isLoading) return (
         <div>
@@ -33,6 +26,3 @@ export default function Home() {
     )
 }
 
-Home.propTypes = {
-    data: PropTypes.array
-};
